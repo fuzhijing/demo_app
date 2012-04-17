ea6d0683d334d2809fd008847351560d1a616f08
U-sz-fuzhijing\fuzhijing <fuzhijing@sz-fuzhijing.aspire.aspire-tech.com>
2012-04-17 18:53:03 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'bf7b4da994d048758a4bfcdd1bf69611921ed9968a67332d417acb6e85e56a4151ecaab8aa3b33079b6b6fa817144d9fc12b5eed0ec4a233245bc9565e7e5bcf'
